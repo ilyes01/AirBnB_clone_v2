06a0f0c58f21fbbafaed18b56c00ec3ed6938673
ilyes01 <ilyesghrab7@gmail.com>
2021-11-04 10:22:31 +0100

    setup fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,3 +7,4 @@ GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
 	IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
 	IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
